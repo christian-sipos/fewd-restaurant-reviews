4ec8fdedd4ae0067b4f31874924970a22a575418
christian-sipos <50636787+christian-sipos@users.noreply.github.com>
2019-09-29 10:43:07 +0200

    fix: service worker delete old versions

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-var staticCacheName = 'restaurant-reviews-static-v';
-var cacheId = Math.floor(Math.random() * 100) + 1;
-staticCacheName += cacheId;
+// set first part of cache name
+var cacheStorage = 'rest-reviews-';
+// create a number for each cache
+var cacheNum = Math.round(Math.random() * 100) + 3;
+// put together complete name if each cache
+cacheStorage += cacheNum;
 
-const urlsToCache = [
+const filesToCache = [
     '/css/styles.css',
     '/data/restaurants.json',
     '/js/dbhelper.js',
-    '/js/sw_register.js',
     '/js/main.js',
     '/js/restaurant_info.js',
+    '/js/sw_reg.js',
     '/img/1.jpg',
     '/img/2.jpg',
     '/img/3.jpg',
@@ -23,50 +26,41 @@ const urlsToCache = [
     '/restaurant.html'
 ];
 
-// in the install callback,
-// 1. open cache
-// 2. add urls 
-self.addEventListener('install', function(event){
-    event.waitUntil(
-        caches.open(staticCacheName)
-        .then(function(cache){
-            return cache.addAll(urlsToCache);
+// react to install event: open cache and add files
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(cacheStorage).then(function(cache) {
+            return cache.addAll(filesToCache);
         })
     );
 });
 
-// manage caches
-self.addEventListener('activate', function(event){
-    event.waitUntil(
-        // get all cache names
-        caches.keys().then(function(cacheNames){
+// react to acitvate event: get list of caches, filter for active cache,
+// delete the other old caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(cacheNames) {
+        return Promise.all(
             cacheNames.filter(function(cacheName) {
-                // return cache
-                 return cacheName.startsWith('restaurant-reviews-') && cacheName != staticCacheName;
-            }).map(function(cacheName){
-                // delete cache
+                return cacheName.startsWith('rest-reviews-') &&
+                       cacheName != cacheStorage;
+            }).map(function(cacheName) {
                 return caches.delete(cacheName);
             })
-        })
-    );
+        );
+    }));
 });
 
-
-self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            // find data and return it
-            if (response) {
+// react to all fetch events
+self.addEventListener('fetch', function(event) {
+    // look in all caches if requests&response exists
+    event.respondWith(caches.match(event.request).then(function(response) {
+        // if found return the response data
+        if (response) return response;
+        // if no match, fetch response data from the network
+        return fetch(event.request).then(function(response) {
+            return caches.open(cacheStorage).then(function(cache) {
+                cache.put(event.request.url, response.clone());
                 return response;
-            }
-
-            // no matching, fetch it from network
-            return fetch(event.request).then(function(response){
-                return caches.open(staticCacheName).then(function(cache){
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                });
             });
-        })
-    );
+        });
+    }));
 });
